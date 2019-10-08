042651f022f0a41b40d6d6e22b5bc1f4a4fc2da6
Andrew Dimmer <contactandrewd@gmail.com>
2019-10-08 14:56:21 -0400

    chore(front-end-web): Stubbed Out Top Level Layouts

---
+++ front-end-web/src/Components/Layouts/NavBar.tsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import { Props } from "../../@Types";
+
+/**
+ * TODO: Add Documentation
+ */
+const NavBar: React.FunctionComponent<Props> = ({
+  state,
+  handlers,
+  classes
+}) => {
+  return (
+    <Fragment>
+      {
+        //TODO
+      }
+    </Fragment>
+  );
+};
+
+export default NavBar;

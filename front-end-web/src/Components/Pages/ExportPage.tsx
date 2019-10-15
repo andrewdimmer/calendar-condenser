dc99490fcece9f066b8cca41f2f87e4de4da8f95
James Lynott <41302574+jcelynott@users.noreply.github.com>
2019-10-15 10:43:53 -0400

    feat(ExportPage): Added export button and text field

--- front-end-web/src/Components/Pages/ExportPage.tsx
+++ front-end-web/src/Components/Pages/ExportPage.tsx
@@ -1,16 +1,28 @@
 import React, { Fragment } from "react";
 import { Props } from "../../@Types";
+import { Button, TextField } from "@material-ui/core";
 
 /**
  * TODO: Add Documentation
  */
+
 const ExportPage: React.FunctionComponent<Props> = ({
   state,
   handlers,
   classes
 }) => {
+  document.cookie = "stage=4";
   return (
     <Fragment>
+      <Button
+        variant="contained"
+        onClick={function() {
+          document.cookie = "stage=4";
+        }}
+      >
+        Export
+      </Button>
+      <TextField variant="standard" label="Calendar name"></TextField>
       {
         //TODO
       }

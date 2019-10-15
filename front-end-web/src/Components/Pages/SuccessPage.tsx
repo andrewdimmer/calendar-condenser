e179a8b49e99c815554b8688c4f8edd641429eb4
James Lynott <41302574+jcelynott@users.noreply.github.com>
2019-10-15 10:46:25 -0400

    feat(SuccessPage): Added check for stage

--- front-end-web/src/Components/Pages/SuccessPage.tsx
+++ front-end-web/src/Components/Pages/SuccessPage.tsx
@@ -1,16 +1,25 @@
 import React, { Fragment } from "react";
 import { Props } from "../../@Types";
-
+import { Typography } from "@material-ui/core";
 /**
  * TODO: Add Documentation
  */
+
+var stage = 4;
 const SuccessPage: React.FunctionComponent<Props> = ({
   state,
   handlers,
   classes
 }) => {
+  var successVal: string;
+  if (document.cookie === "stage=4") {
+    successVal = "Success!";
+  } else {
+    successVal = "Failure!";
+  }
   return (
     <Fragment>
+      <Typography> {successVal} </Typography>
       {
         //TODO
       }

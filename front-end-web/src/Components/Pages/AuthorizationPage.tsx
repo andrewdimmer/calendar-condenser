4def67d4f3fae7b0a02d25bfd5212e13a5e99467
camrongrant1 <56035567+camrongrant1@users.noreply.github.com>
2019-10-18 14:47:48 -0400

    feat(Auhorization Page):Added primary button for login

--- front-end-web/src/Components/Pages/AuthorizationPage.tsx
+++ front-end-web/src/Components/Pages/AuthorizationPage.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import { Props } from "../../@Types";
+import { makeStyles } from "@material-ui/styles";
+import { Button, Typography } from "@material-ui/core";
 
 /**
  * TODO: Add Documentation
@@ -9,11 +11,24 @@ const AuthorizationPage: React.FunctionComponent<Props> = ({
   handlers,
   classes
 }) => {
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: "8px"
+    },
+    input: {
+      display: "none"
+    }
+  }));
+
+  const classes2 = useStyles();
   return (
     <Fragment>
-      {
-        //TODO
-      }
+      <Typography>
+        Press this button to log in to the Authorization page
+      </Typography>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        Primary
+      </Button>
     </Fragment>
   );
 };

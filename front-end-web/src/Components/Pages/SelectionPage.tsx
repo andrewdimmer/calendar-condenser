1b5f3570f6f4838abcee8f37cbc30d2d3542af57
Darius-Student <56035817+Darius-Student@users.noreply.github.com>
2019-10-19 14:51:44 -0400

    Update SelectionPage.tsx
    This is what I got at the moment. I'm not exactly sure where to go from here so as of right now I'm stuck.

--- front-end-web/src/Components/Pages/SelectionPage.tsx
+++ front-end-web/src/Components/Pages/SelectionPage.tsx
@@ -1,18 +1,62 @@
+import { Checkbox, List, ListItem, Button } from "@material-ui/core";
 import React, { Fragment } from "react";
 import { Props } from "../../@Types";
+import { getUserCalendars } from "../../scripts";
 
 /**
  * TODO: Add Documentation
  */
-const SelectionPage: React.FunctionComponent<Props> = ({
-  state,
-  handlers,
-  classes
+interface SelectionProps {
+  calendars: calendar[];
+  selectedCalendars: boolean[];
+  handleSelect: Function;
+  handleSubmit: Function;
+  setStage: Function;
+}
+
+const SelectionPage: React.FunctionComponent<SelectionProps> = ({
+  calendars,
+  selectedCalendars,
+  handleSelect,
+  handleSubmit,
+  setStage
 }) => {
   return (
     <Fragment>
       {
-        //TODO
+        <List>
+          {calendars.map((item: calendar, i: number) => {
+            console.log(i);
+
+            return (
+              <Fragment key={i}>
+                <ListItem
+                  button
+                  onClick={() => {
+                    handleSelect(i);
+                  }}
+                >
+                  <Checkbox
+                    checked={selectedCalendars[i]}
+                    value={calendars[i].key}
+                  />
+
+                  {calendars[i].label}
+                </ListItem>
+              </Fragment>
+            );
+          })}
+
+          <Button
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Next
+          </Button>
+
+          <br />
+        </List>
       }
     </Fragment>
   );

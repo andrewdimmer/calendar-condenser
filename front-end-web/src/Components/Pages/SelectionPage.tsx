689db319811e5b04a3657e443594979a102acd3d
Darius-Student <56035817+Darius-Student@users.noreply.github.com>
2019-10-21 21:13:27 -0400

    fix(SelectionPage): Updated Props

--- front-end-web/src/Components/Pages/SelectionPage.tsx
+++ front-end-web/src/Components/Pages/SelectionPage.tsx
@@ -1,31 +1,20 @@
 import { Checkbox, List, ListItem, Button } from "@material-ui/core";
 import React, { Fragment } from "react";
 import { Props } from "../../@Types";
-import { getUserCalendars } from "../../scripts";
 
 /**
  * TODO: Add Documentation
  */
-interface SelectionProps {
-  calendars: calendar[];
-  selectedCalendars: boolean[];
-  handleSelect: Function;
-  handleSubmit: Function;
-  setStage: Function;
-}
-
-const SelectionPage: React.FunctionComponent<SelectionProps> = ({
-  calendars,
-  selectedCalendars,
-  handleSelect,
-  handleSubmit,
-  setStage
+const SelectionPage: React.FunctionComponent<Props> = ({
+  state,
+  handlers,
+  classes
 }) => {
   return (
     <Fragment>
       {
         <List>
-          {calendars.map((item: calendar, i: number) => {
+          {state.calendars.map((item: any, i: number) => {
             console.log(i);
 
             return (
@@ -33,15 +22,12 @@ const SelectionPage: React.FunctionComponent<SelectionProps> = ({
                 <ListItem
                   button
                   onClick={() => {
-                    handleSelect(i);
+                    handlers.handleSelect(i);
                   }}
                 >
-                  <Checkbox
-                    checked={selectedCalendars[i]}
-                    value={calendars[i].key}
-                  />
+                  <Checkbox checked={state.selectedCalendars[i]} />
 
-                  {calendars[i].label}
+                  {state.calendars[i].summary}
                 </ListItem>
               </Fragment>
             );
@@ -49,7 +35,7 @@ const SelectionPage: React.FunctionComponent<SelectionProps> = ({
 
           <Button
             onClick={() => {
-              handleSubmit();
+              handlers.handleChangeStage(3);
             }}
           >
             Next

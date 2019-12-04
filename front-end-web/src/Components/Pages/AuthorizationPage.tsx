3bfa56fb3df8bcd695d95d17562a62884af60d7e
Darius-Student <56035817+Darius-Student@users.noreply.github.com>
2019-12-04 12:45:10 -0500

    feat(front-end-web): List authorized calendars (#38)
    * feat(front-end-web): Display Authorized Calendars
    
    Working on #33
    
    * fix(front-end-web): Added User Database to Auth Page

--- front-end-web/src/Components/Pages/AuthorizationPage.tsx
+++ front-end-web/src/Components/Pages/AuthorizationPage.tsx
@@ -1,10 +1,20 @@
-import { Button, Typography } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon
+} from "@material-ui/core";
+import { calendar_v3 } from "googleapis";
 import React, { Fragment } from "react";
+import { UserDatabse } from "../../@Types";
+import EditIcon from "@material-ui/icons/Edit";
 
 declare interface AuthorizationProps {
   classes: any;
   handleAuth: () => void;
   handleChangeStage: (newStage: string | number) => void;
+  userDatabase: UserDatabse.Document | null;
 }
 
 /**
@@ -13,14 +23,26 @@ declare interface AuthorizationProps {
 const AuthorizationPage: React.FunctionComponent<AuthorizationProps> = ({
   classes,
   handleAuth,
-  handleChangeStage
+  handleChangeStage,
+  userDatabase
 }) => {
   return (
     <Fragment>
       <Typography>
         Press this button to log in to the Authorization page
       </Typography>
-      {/* TODO: List Calendars that are already authorized here! */}
+      <List>
+        {userDatabase &&
+          userDatabase.accounts.map(({ accountId, label }) => {
+            return (
+              <ListItem key={accountId}>
+                <div>
+                  <Typography variant="h5">{label}</Typography>
+                </div>
+              </ListItem>
+            );
+          })}
+      </List>
       <Button
         variant="contained"
         size="large"
